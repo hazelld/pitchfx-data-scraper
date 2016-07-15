a74056f97c70735cfe00c1105fd5b1fd0c76c186
William Hazell <whazell@mail.uoguelph.ca>
2016-07-15 12:23:16 -0400

    Starting work on getting lineups

--- scripts/scraping_tools/gameday_lineups.pl
+++ scripts/scraping_tools/gameday_lineups.pl
@@ -1,3 +1,5 @@
+#!/bin/perl
+
 #	Author: William Hazell
 #
 #	This script is used for: 
@@ -8,3 +10,23 @@
 #	Parse the needed information and insert into 
 #	the proper database. 
 #
+#	1. Get the current lineup page, parse for lineups
+#	2. Create matchups for each PvB
+#	3. Get their IDs from the players DB
+#	4. Create entry in the matchup DB
+#
+
+use DBI;
+use WWW::Mechanize;
+use HTML::Parser();
+
+my ($month, $day) = @ARGV;
+my $year = "2016";
+my $site = "www.baseballpress.com/lineups/" .$year."-".$month."-".$day;
+
+# Get our site 
+my $mech = WWW::Mechanize->new( autocheck => 0 );
+$mech->get($site);
+
+
+
