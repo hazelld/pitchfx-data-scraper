8aa75aae2673d804bc7b59612c917bf748476306
William Hazell <whazell@mail.uoguelph.ca>
2016-08-05 19:42:38 -0400

    Starting py scraper

--- v1/main.py
+++ v1/main.py
@@ -1,9 +1,10 @@
 #!/bin/python
 
 import logging
-
+from scrapers import dscrape
 
 #   Set up the logger
 logging.basicConfig(filename="mlb.log", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
+dscrape.data_scrape(2015, 07, 07)
