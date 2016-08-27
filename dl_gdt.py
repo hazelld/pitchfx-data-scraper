f9de9e55b51f0a074573253cbc33238f03dc98a6
William Hazell <whazell@mail.uoguelph.ca>
2016-08-27 14:48:51 -0400

    Adding script to mass download gd2 files

---
+++ dl_gdt.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from datetime import timedelta, date
+from scrapers.dscrape import get_page, get_links
+from scrapers.config import *
+import os
+
+start_date = date(2016, 7, 1)
+end_date   = date(2016, 7, 10)
+base_dir   = "gd2/"
+
+if init_globs() == False:
+    print("Couldn't set up globals")
+
+#
+def daterange (start, end):
+    for n in range(int ((end-start).days)):
+        yield start + timedelta(n)
+
+
+for sd in daterange(start_date, end_date):
+    
+    ext     = str(sd.year)+"/month_"+'%02d'%sd.month+"/day_"+'%02d' % sd.day + "/"
+    url     = base_url + ext
+    new_dir = base_dir + "year_" + ext 
+    games   = get_links(url)
+
+    for game in games:
+        path     = new_dir + game
+        game_url = url + "gid_" + game
+
+        if not os.path.exists(path):
+            os.makedirs(path + "inning")
+
+        for f in xml_files:
+            
+            try:
+                xml = get_page(game_url + f)
+            except:
+                print("Could not get page: " + game_url + f)
+                continue 
+
+            print("Writing File: " + os.path.join(path, f))
+            with open(os.path.join(path, f), 'w') as temp_file:
+                temp_file.write(str(xml.read()))
+
+
