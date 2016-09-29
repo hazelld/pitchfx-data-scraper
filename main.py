01569683cfa5f41d59b68796d1d2e0b031f1fa96
William Hazell <whazell@mail.uoguelph.ca>
2016-09-29 13:18:53 -0400

    Started setup process

--- main.py
+++ main.py
@@ -1,23 +1,53 @@
 #!/bin/python
 
 import logging
-from scrapers import pscrape, gd_scrape
+from scrapers import pscrape, gd_scrape, config, db
 from datetime import timedelta
 import datetime
-
-def daterange (start, end):
-    for n in range(int ((end-start).days)):
-        yield start + timedelta(n)
+import time
+import os.path
+import os
+import sys
 
 #   Set up the logger
 logging.basicConfig(filename="mlb.log", level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-date = datetime.date(2016, 7, 8)
-pscrape.pscrape(date)
+todays_date = time.strftime("%d%m%Y")
 
-start = datetime.date(2016, 5, 1)
-end = datetime.date(2016, 6, 1)
+# Check if the config file exists, if not get the db info manually
+if os.path.isfile(config.config_file) == False:
+    print("Config file not found. Need to make one before continuing.")
+    config.make_config_file()
 
-for dt in daterange(start, end):
-    gd_scrape.gd_scrape(dt, "disk")
+# Check if we can connect to the database
+if db.init_db() == False:
+    print("Could not connect to database. Please ensure database exists and try again")
+    sys.exit()
+
+# Check if the proper database tables are available
+if db.check_tables() == False:
+    print("Proper tables are not available in database...")
+    schema = db.get_newest_schema()
+    print("Newest schema is: " + schema + "\nInstalling Schema...")
+    os.system("mysql " + db.get_name() + " < " + schema)
+    print("Schema is installed")
+
+
+# Update player database
+print("Attempting to update the player database")
+pscrape.pscrape(todays_date)
+
+
+# Get the lastest date of game
+# Ask if they would like to update the databases until today
+# Ask if web or disk download
+
+
+def daterange (start, end):
+    ''' 
+        This function allows for iteration over a date range
+        defined by start and end arguments
+    '''
+    for n in range(int ((end-start).days)):
+        yield start + timedelta(n)
