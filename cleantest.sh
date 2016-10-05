d49c2e21ef6b7b3a974c3d92009a197fbcfb7e53
William Hazell <whazell@mail.uoguelph.ca>
2016-10-04 20:38:50 -0400

    Modified cleantest process

--- cleantest.sh
+++ cleantest.sh
@@ -1,5 +1,4 @@
 rm mlb.log
 mysql mlb_stats < db/db_scripts/clear_db.sql
 rm scrapers/scrapers.cfg
-printf "[PlayerScraper]\nlastupdate = 160828" > scrapers/scrapers.cfg
-python3 -m cProfile -s tottime main.py
+python3 main.py
